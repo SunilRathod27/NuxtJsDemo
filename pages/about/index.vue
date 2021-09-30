0d43a45ddb6511641d8fa3a82d163d49f408213a
unknown <shrathod515@gmail.com>
2021-09-30 15:58:10 +0530

    commit

---
+++ pages/about/index.vue
@@ -0,0 +1,18 @@
+<template>
+ <b-card bg-variant="light"  title="About us">
+  <b-card-text>
+    With supporting text below as a natural lead-in to additional content.
+  </b-card-text>
+  <b-button href="#" variant="primary">Go somewhere</b-button>
+</b-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
